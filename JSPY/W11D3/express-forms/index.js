a3f8539efdb049e505f1753832dc947236b500b6
Derek Shue <dshue@berkeley.edu>
2021-03-10 11:09:00 -0800

    Set up guest form

--- JSPY/W11D3/express-forms/index.js
+++ JSPY/W11D3/express-forms/index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.render('index', { title: "Guest List", guests});
 });
 
+app.get('/guest', (req, res) => {
+  res.render('guest-form', { title: "Guest Form" });
+});
+
 const port = 8081;
 
 app.listen(port, () => console.log(`App is listening on port ${port}...`));
